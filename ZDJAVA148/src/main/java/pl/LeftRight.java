10bd14f2c0cb0f326401c5d7d61235a754bfa71c
Mateusz <klinke.mateusz@gmail.com>
2023-09-02 14:43:17 +0200

    pierwszy commit

---
+++ ZDJAVA148/src/main/java/pl/LeftRight.java
@@ -0,0 +1,37 @@
+package pl;
+
+public class LeftRight {
+
+    public class Tuple<L, R> {
+        private L left;
+        private R right;
+
+        public Tuple(L left, R right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public L getLeft() {
+            return left;
+        }
+
+        public R getRight() {
+            return right;
+        }
+
+        public void setLeft(L left) {
+            this.left = left;
+        }
+
+        public void setRight(R right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + left + ", " + right + ")";
+        }
+
+    }
+
+}
